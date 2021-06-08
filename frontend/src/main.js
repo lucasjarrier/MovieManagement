29761677adeed4bb47bf4186e8739037a675bc9f
lucasjarrier <lucas.cavalcanti@ccc.ufcg.edu.br>
2021-06-08 14:39:24 -0300

    adicionando atributos do cadastro de usuario

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import Home from './Home.vue'
 import Element from 'element-ui'
 import router from './routes'
+import locale from 'element-ui/lib/locale/lang/pt-br'
 
 Vue.config.productionTip = false
-Vue.use(Element)
+Vue.use(Element,{locale})
     
 export default new Vue({
     router,
