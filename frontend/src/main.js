217b1ba502566d2605862e344ac09d6e05c5175b
lucasjarrier <lucas.cavalcanti@ccc.ufcg.edu.br>
2021-06-08 08:29:32 -0300

    alterracao vue 2

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import Element from 'element-ui'
 
-createApp(App).mount('#app')
+    
+Vue.config.productionTip = false
+Vue.use(Element)
+    
+new Vue ({
+    render: h => h(App),
+}).$mount('#app')
