460fab45f7ad679a942d505aa2eb46068afb3a28
Ivan Borba <ivan.jrnicolau@gmail.com>
2021-06-09 10:12:25 -0300

    [+] Cadastro de FIlmes de Diretores

--- frontend/src/components/CadastroDiretor.vue
+++ frontend/src/components/CadastroDiretor.vue
@@ -1,14 +1,32 @@
 <template lang="pug">
   div
     h1 Esse é o cadastro de diretor
+    el-row
+      el-cow
+        el-form
+          el-row
+            h3 Nome do diretor:
+            el-input(placeholder="Fulano..." v-model="nomeDiretor").custom-input()
+          el-row
+            h3 Sobrenome do diretor:
+            el-input(placeholder="...da Silva" v-model="sobrenomeDiretor").custom-input()
 </template>
 
 <script>
 export default {
-    name: 'CadastroDiretor'
+    name: 'CadastroDiretor',
+    data() {
+      return {
+        nomeDiretor: '',
+        sobrenomeDiretor: ''
+      }
+    }
 }
 </script>
 
 <style scoped>
+.custom-input{
+  max-width: 40%;
+}
 
 </style>
--- frontend/src/components/CadastroFilme.vue
+++ frontend/src/components/CadastroFilme.vue
@@ -1,14 +1,43 @@
 <template lang="pug">
   div 
-    h1 Esse é o cadastro de filme
+    h1 Cadastro de Filmes
+    el-row
+      el-cow
+        el-form
+          el-row
+            h3 Nome do filme:
+            el-input(placeholder="Digite o nome do filme" v-model="nomeFilme").custom-input()
+          el-row
+            h3 Gênero do filme:
+            el-select(placeholder="Selecione o gênero do filme" v-model="generoFilme")
+              el-option(value='terror' label="Terror")
+              el-option(value='drama' label="Drama")
+              el-option(value='fantasia' label="Fantasia")
+          el-row
+            h3 Data de lançamento:
+            el-date-picker(placeholder="Selecione uma data" v-model="lancamento").custom-input()
+          el-row
+            h3 Nome do diretor:
+            el-input(placeholder="Digite o nome do diretor" v-model="diretor").custom-input()    
 </template>
 
 <script>
 export default {
-    name: 'CadastroFilme'
+    name: 'CadastroFilme',
+    data() {
+      return {
+        nomeFilme: '',
+        generoFilme: '',
+        lancamento: '',
+        diretor: ''
+      }
+    }
 }
 </script>
 
 <style scoped>
+.custom-input{
+  max-width: 40%;
+}
 
 </style>
